ccf8c83013095e509e28c3dba7e40a949b80ad34
Julius Schmidt <newceptiondev@googlemail.com>
2022-08-16 12:49:32 +0200

    Finished Day 23

--- src/day23/task2/Day23Task2.java
+++ src/day23/task2/Day23Task2.java
@@ -1,4 +1,4 @@
-package day23.task1;
+package day23.task2;
 
 import util.InputReader;
 import util.IntCode;
@@ -7,18 +7,21 @@ import util.Pair;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Day23Task1 {
-
+public class Day23Task2 {
     public static void main(String[] args) {
         List<String> in = InputReader.read("src/day23/task1/input.txt");
 
-        new Day23Task1(Arrays.stream(in.get(0).split(",")).map(Long::parseLong).collect(Collectors.toList()));
+        new Day23Task2(Arrays.stream(in.get(0).split(",")).map(Long::parseLong).collect(Collectors.toList()));
     }
 
-    public Day23Task1(List<Long> intcode) {
+    public Day23Task2(List<Long> intcode) {
         Map<Integer, IntCode> machines = new HashMap<>();
         Map<Integer, Queue<Pair<Long, Long>>> packages = new HashMap<>();
         Map<Integer, List<Long>> partialPackages = new HashMap<>();
+        Map<Integer, Integer> idleMachines = new HashMap<>();
+
+        Pair<Long, Long> lastNATPackage = null;
+        Long lastYDeliveredToMachine0ByNAT = null;
 
         for (int i = 0; i < 50; i++) {
             IntCode intCode = new IntCode(intcode);
@@ -27,6 +30,7 @@ public class Day23Task1 {
             machines.put(i, intCode);
             packages.put(i, new LinkedList<>());
             partialPackages.put(i, new ArrayList<>());
+            idleMachines.put(i, 0);
         }
 
         while (true) {
@@ -37,16 +41,18 @@ public class Day23Task1 {
                     machines.get(i).addToInput(inPackage.getValue());
                 }
 
-                Pair<Boolean, Long> output = machines.get(i).doStep(true, false);
+                Pair<Boolean, Long> output = machines.get(i).doStep(true, true);
 
-                if (output != null && output.getValue() != null) {
+                if(output != null && !output.getKey()) {
+                    idleMachines.put(i, idleMachines.get(i) + 1);
+                } else if (output != null && output.getValue() != null) {
+                    idleMachines.put(i, 0);
                     List<Long> currentList = partialPackages.get(i);
                     currentList.add(output.getValue());
 
                     if (currentList.size() == 3) {
                         if (currentList.get(0) != null && currentList.get(0) == 255L) {
-                            System.out.println(currentList.get(2));
-                            return;
+                            lastNATPackage = new Pair<>(currentList.get(1), currentList.get(2));
                         }
 
                         if (packages.containsKey(Math.toIntExact(currentList.get(0)))) {
@@ -56,6 +62,21 @@ public class Day23Task1 {
                         currentList = new ArrayList<>();
                     }
                     partialPackages.put(i, currentList);
+                }
+            }
+
+            if(packages.values().stream().map(Collection::size).noneMatch(size -> size > 0) && partialPackages.values().stream().map(Collection::size).noneMatch(size -> size > 0) && idleMachines.values().stream().filter(val -> val > 200).count() == machines.values().size()) {
+                machines.get(0).addToInput(lastNATPackage.getKey());
+                machines.get(0).addToInput(lastNATPackage.getValue());
+
+                if(lastNATPackage.getValue().equals(lastYDeliveredToMachine0ByNAT)) {
+                    System.out.println(lastYDeliveredToMachine0ByNAT);
+                    return;
+                }
+
+                lastYDeliveredToMachine0ByNAT = lastNATPackage.getValue();
+                for(int i = 0; i < 50; i++) {
+                    idleMachines.put(i, 0);
                 }
             }
         }
--- src/util/IntCode.java
+++ src/util/IntCode.java
@@ -6,11 +6,13 @@ public class IntCode {
 
     private Map<Long, Long> memory;
 
-    private Queue<Integer> input = new LinkedList<>();
+    private Queue<Long> input = new LinkedList<>();
 
     private long relativeBase = 0;
 
     private long currentIndex = 0;
+
+    private long defaultInput = 0;
 
     int j = 0;
 
@@ -25,97 +27,103 @@ public class IntCode {
 
     public Long runCode(boolean haltAtOutput) {
         while (true) {
-//            j++;
-//
-//            if(j >= 50) {
-//                return null;
-//            }
-//            System.out.println("Current Index: " + currentIndex);
-            int opCode = (int) (getMemoryValue(currentIndex) - (Math.floorDiv(getMemoryValue(currentIndex), 100) * 100));
-            List<Integer> parameterMode = new ArrayList<>();
-
-            for (int position = String.valueOf(getMemoryValue(currentIndex)).length() - 3;
-                 position >= 0; position--) {
-                parameterMode.add(Integer.parseInt(String.valueOf(
-                        String.valueOf(getMemoryValue(currentIndex)).charAt(position))));
-            }
-
-            long firstValue = parameterMode.size() < 1 || parameterMode.get(0) == 0
-                    ? getMemoryValue(getMemoryValue(currentIndex + 1))
-                    : parameterMode.get(0) == 1
-                            ? getMemoryValue(currentIndex + 1)
-                            : getMemoryValue(getMemoryValue(currentIndex + 1) + relativeBase);
-            long secondValue = parameterMode.size() < 2 || parameterMode.get(1) == 0
-                    ? getMemoryValue(getMemoryValue(currentIndex + 2))
-                    : parameterMode.get(1) == 1
-                            ? getMemoryValue(currentIndex + 2)
-                            : getMemoryValue(getMemoryValue(currentIndex + 2) + relativeBase);
-            long thirdValue = parameterMode.size() < 3 || parameterMode.get(2) == 0 ? getMemoryValue(
-                    currentIndex + 3) : getMemoryValue(currentIndex + 3) + relativeBase;
-
-            if (opCode == 1 || opCode == 2 || opCode == 7 || opCode == 8) { // 3 Parameter
-                if (opCode == 1) { // Addition
-                    memory.put(thirdValue, firstValue + secondValue);
-//                    System.out.println("add with result: " + memory.get(thirdValue));
-                } else if (opCode == 2) { // Multiplication
-                    memory.put(thirdValue, firstValue * secondValue);
-//                    System.out.println("mult with result: " + memory.get(thirdValue));
-                } else if (opCode == 7) { // Smaller than
-//                    System.out.println("compare less than");
-                    if (firstValue < secondValue) {
-                        memory.put(thirdValue, 1L);
-//                        System.out.println("set " + thirdValue + " to 1");
-                    } else {
-                        memory.put(thirdValue, 0L);
-//                        System.out.println("set " + thirdValue + " to 0");
-                    }
-                } else if (opCode == 8) { // Equal
-//                    System.out.println("equal");
-                    if (firstValue == secondValue) {
-                        memory.put(thirdValue, 1L);
-//                        System.out.println("set " + thirdValue + " to 1");
-                    } else {
-                        memory.put(thirdValue, 0L);
-//                        System.out.println("set " + thirdValue + " to 0");
-                    }
-                }
-                currentIndex += 4;
-            } else if (opCode == 3 || opCode == 4 || opCode == 9) { // 1 Parameter
-                if (opCode == 3) { // Take Input
-                    Integer head = input.poll();
-                    memory.put(parameterMode.size() < 1 || parameterMode.get(0) == 0
-                            ? getMemoryValue(currentIndex + 1) : getMemoryValue(currentIndex + 1) + relativeBase, (long) (head != null ? head : 0));
-                    currentIndex += 2;
-                } else if (opCode == 4) { // Output
-                    currentIndex += 2;
-                    if(haltAtOutput) {
-//                        System.out.println("outputting: " + firstValue + " at " + (currentIndex - 2));
-                        return firstValue;
-                    } else {
-                        System.out.println(firstValue);
-                    }
-                } else if (opCode == 9) { // Adjust relative Base
-                    relativeBase += firstValue;
-                    currentIndex += 2;
-//                    System.out.println("adjusted relative base to " + relativeBase);
-                }
-            } else if (opCode == 5 || opCode == 6) { // 2 Parameter
-                if (opCode == 5) { // Not Zero
-                    if (firstValue != 0) {
-                        currentIndex = secondValue - 3;
-//                        System.out.println("not zero jump to: " + (currentIndex + 3));
-                    }
-                } else if (opCode == 6) { // Is Zero
-                    if (firstValue == 0) {
-                        currentIndex = secondValue - 3;
-//                        System.out.println("zero jump to: " + (currentIndex + 3));
-                    }
-                }
-                currentIndex += 3;
-            } else if (opCode == 99) {
-                return null;
+            Pair<Boolean, Long> result = doStep(haltAtOutput, false);
+            if(result != null) {
+                return result.getValue();
             }
         }
+    }
+
+    public Pair<Boolean, Long> doStep(boolean haltAtOutput, boolean informReadInput) {
+        int opCode = (int) (getMemoryValue(currentIndex) - (Math.floorDiv(getMemoryValue(currentIndex), 100) * 100));
+        List<Integer> parameterMode = new ArrayList<>();
+
+        for (int position = String.valueOf(getMemoryValue(currentIndex)).length() - 3; position >= 0; position--) {
+            parameterMode.add(Integer.parseInt(String.valueOf(String.valueOf(getMemoryValue(currentIndex)).charAt(position))));
+        }
+
+        long firstValue = parameterMode.size() < 1 || parameterMode.get(0) == 0
+                ? getMemoryValue(getMemoryValue(currentIndex + 1))
+                : parameterMode.get(0) == 1 ? getMemoryValue(currentIndex + 1) : getMemoryValue(getMemoryValue(currentIndex + 1) + relativeBase);
+        long secondValue = parameterMode.size() < 2 || parameterMode.get(1) == 0
+                ? getMemoryValue(getMemoryValue(currentIndex + 2))
+                : parameterMode.get(1) == 1 ? getMemoryValue(currentIndex + 2) : getMemoryValue(getMemoryValue(currentIndex + 2) + relativeBase);
+        long thirdValue = parameterMode.size() < 3 || parameterMode.get(2) == 0 ? getMemoryValue(
+                currentIndex + 3) : getMemoryValue(currentIndex + 3) + relativeBase;
+
+        if (opCode == 1 || opCode == 2 || opCode == 7 || opCode == 8) { // 3 Parameter
+            if (opCode == 1) { // Addition
+                memory.put(thirdValue, firstValue + secondValue);
+                //                    System.out.println("add with result: " + memory.get(thirdValue));
+            } else if (opCode == 2) { // Multiplication
+                memory.put(thirdValue, firstValue * secondValue);
+                //                    System.out.println("mult with result: " + memory.get(thirdValue));
+            } else if (opCode == 7) { // Smaller than
+                //                    System.out.println("compare less than");
+                if (firstValue < secondValue) {
+                    memory.put(thirdValue, 1L);
+                    //                        System.out.println("set " + thirdValue + " to 1");
+                } else {
+                    memory.put(thirdValue, 0L);
+                    //                        System.out.println("set " + thirdValue + " to 0");
+                }
+            } else if (opCode == 8) { // Equal
+                //                    System.out.println("equal");
+                if (firstValue == secondValue) {
+                    memory.put(thirdValue, 1L);
+                    //                        System.out.println("set " + thirdValue + " to 1");
+                } else {
+                    memory.put(thirdValue, 0L);
+                    //                        System.out.println("set " + thirdValue + " to 0");
+                }
+            }
+            currentIndex += 4;
+        } else if (opCode == 3 || opCode == 4 || opCode == 9) { // 1 Parameter
+            if (opCode == 3) { // Take Input
+                //                    System.out.println("Reading Input");
+                Long head = input.poll();
+                if (head == null) {
+                    head = defaultInput;
+                }
+
+                memory.put(parameterMode.size() < 1 || parameterMode.get(0) == 0 ? getMemoryValue(
+                                currentIndex + 1) : getMemoryValue(currentIndex + 1) + relativeBase,
+                        head);
+                currentIndex += 2;
+
+                if(informReadInput) {
+                    return new Pair<>(false, null);
+                }
+            } else if (opCode == 4) { // Output
+                currentIndex += 2;
+                if (haltAtOutput) {
+                    //                        System.out.println("outputting: " + firstValue + " at " + (currentIndex - 2));
+                    return new Pair<>(true, firstValue);
+                } else {
+                    System.out.println(firstValue);
+                }
+            } else if (opCode == 9) { // Adjust relative Base
+                relativeBase += firstValue;
+                currentIndex += 2;
+                //                    System.out.println("adjusted relative base to " + relativeBase);
+            }
+        } else if (opCode == 5 || opCode == 6) { // 2 Parameter
+            if (opCode == 5) { // Not Zero
+                if (firstValue != 0) {
+                    currentIndex = secondValue - 3;
+                    //                        System.out.println("not zero jump to: " + (currentIndex + 3));
+                }
+            } else if (opCode == 6) { // Is Zero
+                if (firstValue == 0) {
+                    currentIndex = secondValue - 3;
+                    //                        System.out.println("zero jump to: " + (currentIndex + 3));
+                }
+            }
+            currentIndex += 3;
+        } else if (opCode == 99) {
+            return new Pair<>(true, null);
+        }
+        return null;
     }
 
     public Map<Long, Long> getMemory() {
@@ -126,7 +134,7 @@ public class IntCode {
         this.memory = memory;
     }
 
-    public void addToInput(int toAdd) {
+    public void addToInput(long toAdd) {
         input.offer(toAdd);
     }
 
@@ -136,5 +144,9 @@ public class IntCode {
 
     public void clearInput() {
         input = new LinkedList<>();
+    }
+
+    public void setDefaultInput(long defaultInput) {
+        this.defaultInput = defaultInput;
     }
 }
